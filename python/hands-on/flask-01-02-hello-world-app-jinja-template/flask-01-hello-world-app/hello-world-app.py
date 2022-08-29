72fc1bf2e33874fe25b632b4da43533ee638579e
HIANDIC <leaands25@gmail.com>
2022-08-29 13:26:31 +0200

    modify hello-world.py

--- python/hands-on/flask-01-02-hello-world-app-jinja-template/flask-01-hello-world-app/hello-world-app.py
+++ python/hands-on/flask-01-02-hello-world-app-jinja-template/flask-01-hello-world-app/hello-world-app.py
@@ -2,30 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# it is decorater
-# this is root page
-
-
 @app.route('/')
 def head():
-    return '<h1>Hello World Armando. This is from Clarusway DE</>'
-
-# this is second page
+    return "<h1>Hello World Armando This is from Clarusway DE</h1>"
 
 
 @app.route('/second')
 def second():
-    return 'This is second page'
-
+    return "This is the second page"
 
 @app.route('/third/subthird')
 def third():
-    return 'This is subpage of third page'
+    return "This is the subpage of third page"
 
 
 @app.route('/forth/<string:id>')
 def forth(id):
     return f'Id of this page is {id}'
+
 
 
 if __name__ == '__main__':
